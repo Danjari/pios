78b23728cc7a17452970ba7da91eb83adcdd0aa8
Moudjahid Moussa <mm12515@nyu.edu>
2025-03-13 17:21:21 +0400

    downgraded next

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,19 @@
 import { withPayload } from "@payloadcms/next/withPayload";
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  webpack:(config)=>{
-    config.resolve.fallback = {
-      fs: false,
-      worker_threads:false,
-      readline: false,
-    };
-    return config;
-  }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      { source: "/rss.xml", destination: "/feed/rss.xml" },
+      { source: "/atom.xml", destination: "/feed/atom.xml" },
+      { source: "/feed.json", destination: "/feed/feed.json" },
+      { source: "/rss", destination: "/feed/rss.xml" },
+      { source: "/feed", destination: "/feed/rss.xml" },
+      { source: "/atom", destination: "/feed/atom.xml" },
+      { source: "/json", destination: "/feed/feed.json" },
+    ];
+  },
 };
 
 export default withPayload(nextConfig);
