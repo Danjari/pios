61ac2254c7ef750bf86f3aee63797c48a4975947
Moudjahid Moussa <mm12515@nyu.edu>
2025-03-13 16:51:33 +0400

    removed fs, worker_thread and readline from frontend

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack:(config)=>{
+    config.resolve.fallback = {
+      fs: false,
+      worker_threads:false,
+      readline: false,
+    };
+    return config;
+  }
 };
 
 export default withPayload(nextConfig);
