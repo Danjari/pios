670a2aa25383166fc04db91d844cf9a5a3a4fcc1
Moudjahid Moussa <mm12515@nyu.edu>
2025-07-25 15:50:23 +0100

    add NavBar and Footer components to LandingPage for improved layout and structure

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -4,13 +4,19 @@ import Hero from "@/components/marketing/hero";
 import Landingfiliere from "@/components/marketing/Landingfiliere";
 import LandingUniv from "@/components/marketing/LandingUniv";
 import LandingBourse from "@/components/marketing/LandingBourse";
+import NavBar from "@/components/marketing/navBar";
+import Footer from "@/components/marketing/footer";
 export default function LandingPage() {
   return (
+    <>
+    <NavBar/>
     <div>
       <Hero />
       <LandingBourse />
       <Landingfiliere />
       <LandingUniv />
     </div>
-  );
+    <Footer/>
+    </>
+    );
 }
