9450903435a64d807eae1ea62e712322a5826b62
Moudjahid Moussa <mm12515@nyu.edu>
2025-07-25 15:51:30 +0100

    add NavBar and Footer components to Filieres page for improved layout and navigation

--- app/(frontend)/filieres/page.tsx
+++ app/(frontend)/filieres/page.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import HomeClient from "../../../components/homeClient";  // Separate client component
 
 import { fetchFilieres } from "@/lib/api";
+import NavBar from "@/components/marketing/navBar";
+import Footer from "@/components/marketing/footer";
 //export const revalidate = 60;
 
 
@@ -11,9 +13,13 @@ export default async function Home() {
   const filieres = await fetchFilieres(); // Fetch from API
 
   return (
+    <>
+    <NavBar/>
     <div>
       {/* Pass fetched data to client component */}
       <HomeClient filieres={filieres} />
     </div>
-  );
+    <Footer/>
+    </>
+    );
 }
